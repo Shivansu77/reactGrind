72eb4455e932831f7d0eff9b47b1f9b666fafad2
shivansu77 <shivansubisht77@gmail.com>
2025-12-05 13:09:10 +0530

    adding count of like

--- frontend/src/components/User.js
+++ frontend/src/components/User.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import {useState} from 'react';
 const User = ({name})=>{
+     const [count, setCount] = useState(0);
     return <div className="user-card">
         <h1>Name : {name}</h1>
         <h3>Location : Jalandhar</h3>
         <h4>Contact : @Shark77su</h4>
+        <h4>Count : {count}</h4>
+        <button onClick={() => setCount(count + 1)}>Like</button>
     </div>
 }
 export default User;
