0f6a5e87d0012dcc01ed8653fbfa1c432d2971c2
shivansu77 <shivansubisht77@gmail.com>
2025-12-05 13:09:30 +0530

    add using class component

--- frontend/src/components/UserClass.js
+++ frontend/src/components/UserClass.js
@@ -3,16 +3,28 @@ import React from 'react';
 class UserClass extends React.Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      count: 0
+    };
   }
 
   render() {
     const { name, location } = this.props;
+    const { count } = this.state;
 
     return (
       <div className="user-card">
         <h1>Name : {name}</h1>
         <h3>Location : {location}</h3>
-        <h4>Contact : @Shark77su</h4>
+        <h4>Contact : @Shark77su</h4>   
+        <h4>Count : {count}</h4>
+
+        <button 
+          onClick={() => this.setState({ count: count + 1 })}
+        >
+          Like
+        </button>
       </div>
     );
   }
