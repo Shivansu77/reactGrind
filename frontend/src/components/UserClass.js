5710a8f8dd1efd7479a70056b50d5f0c5fea9d14
shivansu77 <shivansubisht77@gmail.com>
2025-12-05 12:10:29 +0530

    ADDED Location

--- frontend/src/components/UserClass.js
+++ frontend/src/components/UserClass.js
@@ -1,18 +1,17 @@
 import React from 'react';
 
 class UserClass extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            count : 0,
-            count2 : 1
-        }
-    }
+  constructor(props) {
+    super(props);
+  }
+
   render() {
+    const { name, location } = this.props;
+
     return (
       <div className="user-card">
-        <h1>Name : {this.props.name}</h1>
-        <h3>Location : Jalandhar</h3>
+        <h1>Name : {name}</h1>
+        <h3>Location : {location}</h3>
         <h4>Contact : @Shark77su</h4>
       </div>
     );
