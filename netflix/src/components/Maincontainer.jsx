26a0e3dc7494f208ce3e24aee44f2c6f7e6eb774
Shivansu77 <shivansubisht77@gmail.com>
2026-01-14 19:46:03 +0530

    creating videoBackground

--- netflix/src/components/Maincontainer.jsx
+++ netflix/src/components/Maincontainer.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
+import Videotitle from './Videotitle'
+import VideoBackground from './VideoBackground'
 
 const Maincontainer = () => {
-  return (
-    <div className="px-8 py-6 text-white text-2xl font-semibold">
-      Maincontainer
+    const movies = useSelector(store=> store.movies?.nowPlayingMovies)
+    const mainMovie = movies ? movies[0] : null;
+    return (
+    <div >
+        <Videotitle />
+        <VideoBackground />
     </div>
   )
 }
---
+++ netflix/src/components/Videotitle.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Videotitle = () => {
+  return (
+    <div>Videotitle</div>
+  )
+}
+
+export default Videotitle
